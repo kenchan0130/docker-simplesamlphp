650b4b64c18621b453b66e63a95909543b30545e
Tadayuki Onishi <tt.tanishi100@gmail.com>
2019-12-23 19:10:57 +0900

    fix cheking enviroment variables

--- config/simplesamlphp/saml20-sp-remote.php
+++ config/simplesamlphp/saml20-sp-remote.php
@@ -4,8 +4,13 @@
  *
  * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-sp-remote
  */
-assert(getenv('SIMPLESAMLPHP_SP_ENTITY_ID'));
-assert(getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'));
+
+if (!getenv('SIMPLESAMLPHP_SP_ENTITY_ID')) {
+    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ENTITY_ID is not defined as an environment variable.');
+}
+if (!getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
+    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE is not defined as an environment variable.');
+}
 
 $metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
     'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
